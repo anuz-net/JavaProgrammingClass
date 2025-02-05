76e9c0d2eac24b69b12f2d3381ccecdd6c0d7e20
anuz <95576641+anuz-net@users.noreply.github.com>
2025-02-05 12:01:42 +0545

    Updated StackOperation

---
+++ src/StackClass.java
@@ -0,0 +1,17 @@
+public class StackClass {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            next=null;
+        }
+    }
+    static class Stack{
+
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}

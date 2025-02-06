5c874d37d5049d7aeed0544f759c2fb3b29d6cc0
anuz <95576641+anuz-net@users.noreply.github.com>
2025-02-06 12:56:45 +0545

    Added Second Largest V2
    Ad

--- src/SecondLarge.java
+++ src/SecondLarge.java
@@ -1,4 +1,4 @@
-public class SecondLargest {
+public class SecondLarge {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int largest = arr[0];

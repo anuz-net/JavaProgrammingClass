fba12c238deffbf80aea9229ce44de3ef92990e0
anuz <95576641+anuz-net@users.noreply.github.com>
2025-02-20 08:09:39 +0545

    Update Factorial.java

--- ProgramSet2/Factorial.java
+++ ProgramSet2/Factorial.java
@@ -1,1 +1,19 @@
 // 12.	Write a Java program to calculate the factorial of a number using a while loop. 
+package ProgramSet2;
+import java.util.*;
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Positive Number : ");
+        int num = sc.nextInt();
+        Factorial cf = new Factorial();
+        cf.CheckFactorial(num);
+        sc.close();
+    }
+    public void CheckFactorial(int num){
+        int i =0;
+        int fact=1;
+    }
+
+}
